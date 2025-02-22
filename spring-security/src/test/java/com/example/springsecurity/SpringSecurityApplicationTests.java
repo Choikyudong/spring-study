ade2aa6810d5fd511de5e17c62b7f310e24e7b83
Choikyudong <slgmgm@naver.com>
2025-02-22 17:01:01 +0900

    refactor: admin role 적용 및 테스트
    - 트랜잭션을 클래스에 적용시켜 테스트 아이디 중복 방지

--- spring-security/src/test/java/com/example/springsecurity/SpringSecurityApplicationTests.java
+++ spring-security/src/test/java/com/example/springsecurity/SpringSecurityApplicationTests.java
@@ -17,6 +17,7 @@ import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithAnonymousUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -25,6 +26,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@Transactional
 @AutoConfigureMockMvc
 class SpringSecurityApplicationTests {
 
